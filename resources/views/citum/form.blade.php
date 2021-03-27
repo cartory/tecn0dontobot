c7f51b178b2146d117c33b3095e22763d6fce16a
pablotardio <pablo.tv30@gmail.com>
2021-03-27 19:48:36 -0400

    making citas selector

--- resources/views/citum/form.blade.php
+++ resources/views/citum/form.blade.php
@@ -91,7 +91,10 @@
 <div class="box box-info padding-1">
     
     <div class="box-body">
-       
+      @php
+        use App\Models\Paciente; 
+        use App\Models\Agenda; 
+      @endphp
         <div class="form-group">
             {{ Form::label('horaInicio') }}
             {{ Form::time('horaInicio', $citum->horaInicio, ['class' => 'form-control' . ($errors->has('horaInicio') ? ' is-invalid' : ''), 'placeholder' => 'Horainicio']) }}
@@ -109,12 +112,12 @@
       </div>
         <div class="form-group">
             {{ Form::label('Pacienteid') }}
-            {{ Form::text('Pacienteid', $citum->Pacienteid, ['class' => 'form-control' . ($errors->has('Pacienteid') ? ' is-invalid' : ''), 'placeholder' => 'Pacienteid']) }}
+            {{ Form::select('Pacienteid' ,[], ['class' => 'form-control' . ($errors->has('Pacienteid') ? ' is-invalid' : ''), 'placeholder' => 'Pacienteid']) }}
             {!! $errors->first('Pacienteid', '<div class="invalid-feedback">:message</p>') !!}
         </div>
         <div class="form-group">
             {{ Form::label('Agendaid') }}
-            {{ Form::text('Agendaid', $citum->Agendaid, ['class' => 'form-control' . ($errors->has('Agendaid') ? ' is-invalid' : ''), 'placeholder' => 'Agendaid']) }}
+            {{ Form::select('Agendaid', [], ['class' => 'form-control' . ($errors->has('Agendaid') ? ' is-invalid' : ''), 'placeholder' => 'Agendaid']) }}
             {!! $errors->first('Agendaid', '<div class="invalid-feedback">:message</p>') !!}
         </div>
 
