520fd3a54a9d1e7c205876ea340fce50b42ee093
Leonardo Añez Vladimirovna <toborochi98@outlook.com>
2021-03-29 00:12:39 -0400

    Motor

--- resources/views/odontologo/form.blade.php
+++ resources/views/odontologo/form.blade.php
@@ -1,6 +1,6 @@
 <div class="box box-info padding-1">
     <div class="box-body">
-        
+
         <div class="form-group">
             {{ Form::label('ci') }}
             {{ Form::number('ci', $odontologo->ci, ['class' => 'form-control' . ($errors->has('ci') ? ' is-invalid' : ''), 'placeholder' => 'Ci']) }}
@@ -27,11 +27,11 @@
             {!! $errors->first('genero', '<div class="invalid-feedback">:message</p>') !!}
         </div>
         @php
-             use App\Models\User; 
+             use App\Models\User;
         @endphp
         <div class="form-group">
             {{ Form::label('Email de usuario') }}
-            {{ Form::select('Usuarioid',User::all()->pluck('email'), $odontologo->Usuarioid, ['class' => 'form-control' . ($errors->has('Usuarioid') ? ' is-invalid' : ''), 'placeholder' => 'Usuarioid']) }}
+            {{ Form::select('Usuarioid',User::all()->pluck('email','id'), $odontologo->Usuarioid, ['class' => 'form-control' . ($errors->has('Usuarioid') ? ' is-invalid' : ''), 'placeholder' => 'Usuarioid']) }}
             {!! $errors->first('Usuarioid', '<div class="invalid-feedback">:message</p>') !!}
         </div>
 
